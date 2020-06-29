a2cc5f8a6a08d1f2a397ce913486273ec4e2c529
gouyz <413977349@qq.com>
2020-06-29 21:15:13 +0800

    111

--- hoopeu/Classes/Tool/network/GYZNetWork.swift
+++ hoopeu/Classes/Tool/network/GYZNetWork.swift
@@ -13,18 +13,18 @@ import MBProgressHUD
 
 /// 网络请求基地址
 #if DEBUG
-//let BaseRequestURL = "http://121.43.122.86/app/"
-///// mqtt host
-//let kDefaultMQTTHost = "121.43.122.86"
-let kDefaultMQTTHost = "119.29.107.14"
-let BaseRequestURL = "http://www.hoopeurobot.com/app/"
+let BaseRequestURL = "http://121.43.122.86/app/"
+/// mqtt host
+let kDefaultMQTTHost = "121.43.122.86"
+//let kDefaultMQTTHost = "119.29.107.14"
+//let BaseRequestURL = "http://www.hoopeurobot.com/app/"
 #else
 /// mqtt host
-//let BaseRequestURL = "http://121.43.122.86/app/"
-///// mqtt host
-//let kDefaultMQTTHost = "121.43.122.86"
-let kDefaultMQTTHost = "119.29.107.14"
-let BaseRequestURL = "http://www.hoopeurobot.com/app/"
+let BaseRequestURL = "http://121.43.122.86/app/"
+/// mqtt host
+let kDefaultMQTTHost = "121.43.122.86"
+//let kDefaultMQTTHost = "119.29.107.14"
+//let BaseRequestURL = "http://www.hoopeurobot.com/app/"
 #endif
 
 class GYZNetWork: NSObject {
